974274ebb0a8ece7325bf8f93969f22f00576a27
luizleiteoliveira <luiz.leite@hotmart.com.br>
2017-11-25 10:34:20 -0200

    find all scenarios by feature

--- nalu-api/src/main/java/com/luizleiteolveira/nalu/repository/ScenarioRepository.java
+++ nalu-api/src/main/java/com/luizleiteolveira/nalu/repository/ScenarioRepository.java
@@ -1,7 +1,12 @@
 package com.luizleiteolveira.nalu.repository;
 
+import com.luizleiteolveira.nalu.entity.Feature;
 import com.luizleiteolveira.nalu.entity.Scenario;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.List;
+
 public interface ScenarioRepository extends PagingAndSortingRepository<Scenario, Long> {
+
+    List<Scenario> findAllByFeature(Feature feature);
 }
