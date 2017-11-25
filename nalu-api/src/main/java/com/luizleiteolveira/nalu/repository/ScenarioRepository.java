366cbda963453f9b00cbb4caecd58551944699f7
luizleiteoliveira <luiz.leite@hotmart.com.br>
2017-11-25 10:32:38 -0200

    create scenario repository

---
+++ nalu-api/src/main/java/com/luizleiteolveira/nalu/repository/ScenarioRepository.java
@@ -0,0 +1,7 @@
+package com.luizleiteolveira.nalu.repository;
+
+import com.luizleiteolveira.nalu.entity.Scenario;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ScenarioRepository extends PagingAndSortingRepository<Scenario, Long> {
+}
