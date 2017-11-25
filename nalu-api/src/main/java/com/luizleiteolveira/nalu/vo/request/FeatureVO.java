ba2077b09044a1079abc56dcdd055fa39b92132e
luizleiteoliveira <luiz.leite@hotmart.com.br>
2017-11-25 10:52:42 -0200

    add fields on VO

--- nalu-api/src/main/java/com/luizleiteolveira/nalu/vo/request/FeatureVO.java
+++ nalu-api/src/main/java/com/luizleiteolveira/nalu/vo/request/FeatureVO.java
@@ -3,7 +3,12 @@ package com.luizleiteolveira.nalu.vo.request;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
-public class FeatureVO {
+public class FeatureVO implements Serializable{
+    private Long id;
+    private String description;
+    private String urlForTest;
 }
