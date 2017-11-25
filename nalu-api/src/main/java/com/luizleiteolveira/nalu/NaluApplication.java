f54eb833c7194f3752f2fda9b9b9a29268f4e6c6
luizleiteoliveira <luiz.leite@hotmart.com.br>
2017-11-25 07:43:49 -0200

    start from backend app

---
+++ nalu-api/src/main/java/com/luizleiteolveira/nalu/NaluApplication.java
@@ -0,0 +1,12 @@
+package com.luizleiteolveira.nalu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaluApplication.class, args);
+	}
+}
