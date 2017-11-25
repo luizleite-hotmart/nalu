da11e4b41838c81479125af379f4cfa5aa976d66
luizleiteoliveira <luiz.leite@hotmart.com.br>
2017-11-25 10:32:48 -0200

    create feature repository

---
+++ nalu-api/src/main/java/com/luizleiteolveira/nalu/repository/FeatureRepository.java
@@ -0,0 +1,8 @@
+package com.luizleiteolveira.nalu.repository;
+
+import com.luizleiteolveira.nalu.entity.Feature;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface FeatureRepository extends PagingAndSortingRepository<Feature, Long> {
+}
+
