a6260e91968bff0f1c43410939b8b20a34189df5
luizleiteoliveira <luiz.leite@hotmart.com.br>
2017-11-25 11:26:22 -0200

    create_method_find_all_features

--- nalu-api/src/main/java/com/luizleiteolveira/nalu/service/FeatureService.java
+++ nalu-api/src/main/java/com/luizleiteolveira/nalu/service/FeatureService.java
@@ -1,10 +1,17 @@
 package com.luizleiteolveira.nalu.service;
 
 
+import com.luizleiteolveira.nalu.entity.Feature;
 import com.luizleiteolveira.nalu.repository.FeatureRepository;
 import com.luizleiteolveira.nalu.repository.ScenarioRepository;
+import com.luizleiteolveira.nalu.vo.response.FeatureResponseVO;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Type;
+import java.util.List;
 
 @Service
 public class FeatureService {
@@ -14,5 +21,14 @@ public class FeatureService {
 
     @Autowired
     private ScenarioRepository scenarioRepository;
+
+    private ModelMapper mapper;
+
+    public List<FeatureResponseVO> findActiveFeatures() {
+        List<Feature> featureList = featureRepository.findAllByActiveIsTrue();
+        Type targetListType = new TypeToken<List<FeatureResponseVO>>() {}.getType();
+        List<FeatureResponseVO> featureResponseVOList = mapper.map(featureList, targetListType);
+        return featureResponseVOList;
+    }
 
 }
