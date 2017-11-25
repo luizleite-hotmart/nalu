e6ee36bcf11e4da96cf2bc817c34fcdcb640eff2
luizleiteoliveira <luiz.leite@hotmart.com.br>
2017-11-25 10:14:59 -0200

    missing properties

--- nalu-api/src/main/java/entity/Scenario.java
+++ nalu-api/src/main/java/entity/Scenario.java
@@ -4,16 +4,20 @@ import lombok.Data;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.List;
 
 @Data
+@Entity
+@Table(name = "scenario")
 public class Scenario implements Serializable {
 
     @Id
