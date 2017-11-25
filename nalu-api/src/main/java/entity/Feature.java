d0c923e54c51913d4861a83cc6ac7ee997d8298c
luizleiteoliveira <luiz.leite@hotmart.com.br>
2017-11-25 08:16:27 -0200

    add_atributes_on_feature

--- nalu-api/src/main/java/entity/Feature.java
+++ nalu-api/src/main/java/entity/Feature.java
@@ -3,9 +3,13 @@ package entity;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.List;
 
 @Data
 public class Feature implements Serializable{
 
+    private Long id;
+    private String description;
+    private List<Scenario> scenarios;
 
 }
