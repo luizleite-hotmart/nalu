b594dc5c0691e140b3ceefd6d172178f67dddddf
luizleiteoliveira <luiz.leite@hotmart.com.br>
2017-11-25 09:28:28 -0200

    fields_from_class

--- nalu-api/src/main/java/entity/FeatureAttribute.java
+++ nalu-api/src/main/java/entity/FeatureAttribute.java
@@ -3,12 +3,29 @@ package entity;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 @Data
 @Entity
 @Table(name = "feature_attribute")
 public class FeatureAttribute implements Serializable{
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "feature_id", nullable = false)
+    private Feature feature;
 }
