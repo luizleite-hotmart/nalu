113a289ec05bae9b3b66a2d069ce6b3a4063e415
luizleiteoliveira <luiz.leite@hotmart.com.br>
2017-11-25 12:14:20 -0200

    remove unused

--- nalu-api/src/main/java/com/luizleiteolveira/nalu/service/FeatureService.java
+++ nalu-api/src/main/java/com/luizleiteolveira/nalu/service/FeatureService.java
@@ -5,12 +5,9 @@ import com.luizleiteolveira.nalu.entity.Feature;
 import com.luizleiteolveira.nalu.repository.FeatureRepository;
 import com.luizleiteolveira.nalu.repository.ScenarioRepository;
 import com.luizleiteolveira.nalu.vo.response.FeatureResponseVO;
-import org.modelmapper.ModelMapper;
-import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,9 +19,6 @@ public class FeatureService {
 
     @Autowired
     private ScenarioRepository scenarioRepository;
-
-    @Autowired
-    private ModelMapper mapper;
 
     public List<FeatureResponseVO> findActiveFeatures() {
         List<Feature> featureList = featureRepository.findAllByActiveIsTrue();
