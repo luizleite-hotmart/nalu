d1ddc5ac2afb83041420bdb6eec3fcf564121c01
luizleiteoliveira <luiz.leite@hotmart.com.br>
2017-11-25 10:49:30 -0200

    add service anotation

--- nalu-api/src/main/java/com/luizleiteolveira/nalu/service/FeatureService.java
+++ nalu-api/src/main/java/com/luizleiteolveira/nalu/service/FeatureService.java
@@ -4,7 +4,9 @@ package com.luizleiteolveira.nalu.service;
 import com.luizleiteolveira.nalu.repository.FeatureRepository;
 import com.luizleiteolveira.nalu.repository.ScenarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class FeatureService {
 
     @Autowired
