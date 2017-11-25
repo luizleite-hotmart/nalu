7a0545c1bdaf012064ba8e492675bba6475751e6
luizleiteoliveira <luiz.leite@hotmart.com.br>
2017-11-25 11:37:28 -0200

    create tests for feature

---
+++ nalu-api/src/test/java/com/luizleiteolveira/nalu/service/FeatureServiceTest.java
@@ -0,0 +1,12 @@
+package com.luizleiteolveira.nalu.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FeatureServiceTest {
+
+    @Test
+    public void findActiveFeatures() {
+    }
+}
