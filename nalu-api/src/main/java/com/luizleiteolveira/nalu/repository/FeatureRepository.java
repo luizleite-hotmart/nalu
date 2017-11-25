e618643323e1ddba485f6c39ce6e5c7db91a9034
luizleiteoliveira <luiz.leite@hotmart.com.br>
2017-11-25 11:26:01 -0200

    create_find_all_on_feature_repository

--- nalu-api/src/main/java/com/luizleiteolveira/nalu/repository/FeatureRepository.java
+++ nalu-api/src/main/java/com/luizleiteolveira/nalu/repository/FeatureRepository.java
@@ -3,6 +3,10 @@ package com.luizleiteolveira.nalu.repository;
 import com.luizleiteolveira.nalu.entity.Feature;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.List;
+
 public interface FeatureRepository extends PagingAndSortingRepository<Feature, Long> {
+
+    List<Feature> findAllByActiveIsTrue();
 }
 
