ed0f8eafa39c32ea3d13368679661782e85d2cda
luizleiteoliveira <luiz.leite@hotmart.com.br>
2017-11-25 10:14:24 -0200

    complete_class

--- nalu-api/src/main/java/entity/Scenario.java
+++ nalu-api/src/main/java/entity/Scenario.java
@@ -2,13 +2,31 @@ package entity;
 
 import lombok.Data;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
+import java.util.List;
 
 @Data
 public class Scenario implements Serializable {
 
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Boolean baseScenario;
+    @Column(name = "base_scenario")
+    private Boolean baseScenario = Boolean.FALSE;
+    @Column(name = "description")
     private String description;
+    @NotNull
+    @JoinColumn(name = "feature_id", nullable = false)
     private Feature feature;
+    @OneToMany(mappedBy = "scenario", cascade = CascadeType.ALL)
+    private List<ScenarioAtribute> scenarioAtributeList;
 }
