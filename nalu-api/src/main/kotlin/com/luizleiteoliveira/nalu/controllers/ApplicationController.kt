ca93399d2127c061365dadf78d956d221bdedbed
luizleite <luiz.leite@hotmart.com.br>
2018-04-23 20:01:51 -0300

    change_controller

--- nalu-api/src/main/kotlin/com/luizleiteoliveira/nalu/controllers/ApplicationController.kt
+++ nalu-api/src/main/kotlin/com/luizleiteoliveira/nalu/controllers/ApplicationController.kt
@@ -1,6 +1,7 @@
 package com.luizleiteoliveira.nalu.controllers
 
 import com.luizleiteoliveira.nalu.entities.Application
+import com.luizleiteoliveira.nalu.entities.Applications
 import kotlinx.nosql.mongodb.MongoDB
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
@@ -12,8 +13,9 @@ class ApplicationController @Autowired constructor(val db: MongoDB){
     @RequestMapping(value = "/add" , method = arrayOf(RequestMethod.POST))
     fun add(@RequestParam("name") name: String): String {
         db.withSession {
-            Application.in
+            Applications.insert(Application(name, true))
         }
+        return "redirect:/owners/"
     }
 
 }
