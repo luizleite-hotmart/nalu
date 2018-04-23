d75a83d63673d29a6b820c06edc27b5f091cff78
luizleite <luiz.leite@hotmart.com.br>
2018-04-23 19:25:06 -0300

    add_mong

--- nalu-api/src/main/kotlin/com/luizleiteoliveira/nalu/controllers/ApplicationController.kt
+++ nalu-api/src/main/kotlin/com/luizleiteoliveira/nalu/controllers/ApplicationController.kt
@@ -1,6 +1,5 @@
-package com.luizleiteoliveira.nalu.controller
+package com.luizleiteoliveira.nalu.controllers
 
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
