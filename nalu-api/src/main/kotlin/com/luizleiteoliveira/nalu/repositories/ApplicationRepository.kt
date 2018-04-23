694175ca6603222b06e39c7830f9eb9e6e12bab5
luizleite <luiz.leite@hotmart.com.br>
2018-04-23 19:31:07 -0300

    create fun return active apps

--- nalu-api/src/main/kotlin/com/luizleiteoliveira/nalu/repositories/ApplicationRepository.kt
+++ nalu-api/src/main/kotlin/com/luizleiteoliveira/nalu/repositories/ApplicationRepository.kt
@@ -1,2 +1,11 @@
 package com.luizleiteoliveira.nalu.repositories
 
+import com.luizleiteoliveira.nalu.entities.Application
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.mongodb.repository.MongoRepository
+
+interface  ApplicationRepository : MongoRepository<Application, Long> {
+
+    fun findAllActiveApplications(pageable: Pageable): Page<Application>
+}
