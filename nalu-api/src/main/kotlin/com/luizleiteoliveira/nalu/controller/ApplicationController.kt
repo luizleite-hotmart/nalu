492cd832e37baedac62c4f3f965a09d950dbb9fb
luizleite <luiz.leite@hotmart.com.br>
2018-04-23 19:22:18 -0300

    create_controller

---
+++ nalu-api/src/main/kotlin/com/luizleiteoliveira/nalu/controller/ApplicationController.kt
@@ -0,0 +1,9 @@
+package com.luizleiteoliveira.nalu.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class ApplicationController {
+
+}
