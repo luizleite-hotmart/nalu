a4b95ae7f11d3cd7d1e2bae6eb1fc01eeb9f0584
luizleite <luiz.leite@hotmart.com.br>
2018-04-23 19:48:07 -0300

    add_mongo_db

--- nalu-api/src/main/kotlin/com/luizleiteoliveira/nalu/controllers/ApplicationController.kt
+++ nalu-api/src/main/kotlin/com/luizleiteoliveira/nalu/controllers/ApplicationController.kt
@@ -1,8 +1,16 @@
 package com.luizleiteoliveira.nalu.controllers
 
-import org.springframework.web.bind.annotation.RestController
+import com.luizleiteoliveira.nalu.entities.Application
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.*
 
 @RestController
-class ApplicationController {
+@RequestMapping("/applications")
+class ApplicationController @Autowired constructor(val db: MongoDB){
+
+    @RequestMapping(value = "/add" , method = arrayOf(RequestMethod.POST))
+    fun add(@RequestParam("name") name: String): String {
+        db.
+    }
 
 }
