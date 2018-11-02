6e86ef07dae7c228d20b6e68604f2e209fcacad6
luizleite-hotmart <luiz.leite@hotmart.com>
2018-11-02 11:48:41 -0300

    remove things that I dont remember why they are here

--- nalu-api/src/main/kotlin/com/luizleiteoliveira/nalu/controllers/ApplicationController.kt
+++ nalu-api/src/main/kotlin/com/luizleiteoliveira/nalu/controllers/ApplicationController.kt
@@ -1,21 +1,15 @@
 package com.luizleiteoliveira.nalu.controllers
 
-import com.luizleiteoliveira.nalu.entities.Application
-import com.luizleiteoliveira.nalu.entities.Applications
-import kotlinx.nosql.mongodb.MongoDB
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/applications")
-class ApplicationController @Autowired constructor(val db: MongoDB){
+class ApplicationController @Autowired constructor(){
 
     @RequestMapping(value = "/add" , method = arrayOf(RequestMethod.POST))
     fun add(@RequestParam("name") name: String): String {
-        db.withSession {
-            Applications.insert(Application(name, true))
-        }
-        return "redirect:/owners/"
+        return "OK"
     }
 
 }
--- nalu-api/src/main/kotlin/com/luizleiteoliveira/nalu/services/ApplicationService.kt
+++
@@ -1,2 +0,0 @@
-package com.luizleiteoliveira.nalu.services
-
