bdafaa9f2e126b6c47174a661fb45bdfa9de7b9d
luizleite-hotmart <luiz.leite@hotmart.com>
2018-11-02 11:35:33 -0300

    create class for teams

--- nalu-api/src/main/kotlin/com/luizleiteoliveira/nalu/entities/Team.kt
+++ nalu-api/src/main/kotlin/com/luizleiteoliveira/nalu/entities/Team.kt
@@ -1,2 +1,13 @@
 package com.luizleiteoliveira.nalu.entities
 
+import javax.persistence.*
+
+@Entity
+data class Team(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        val id: Long,
+
+        @Column(nullable = false)
+        val name: String
+)
