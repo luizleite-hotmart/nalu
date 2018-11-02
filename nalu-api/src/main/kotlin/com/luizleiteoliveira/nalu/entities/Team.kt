e83a46c504b9bf8ea781faff538cd35f87d349f0
luizleite-hotmart <luiz.leite@hotmart.com>
2018-11-02 11:38:33 -0300

    create class mongo like

--- nalu-api/src/main/kotlin/com/luizleiteoliveira/nalu/entities/Team.kt
+++ nalu-api/src/main/kotlin/com/luizleiteoliveira/nalu/entities/Team.kt
@@ -1,13 +1,7 @@
 package com.luizleiteoliveira.nalu.entities
 
-import javax.persistence.*
-
-@Entity
 data class Team(
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        val id: Long,
 
-        @Column(nullable = false)
+        val id: Long,
         val name: String
 )
