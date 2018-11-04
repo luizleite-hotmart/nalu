9f0ecafd2dc3b50d43cd9ef12912a8fcc5dbe35d
luizleite-hotmart <luiz.leite@hotmart.com>
2018-11-04 19:24:31 -0200

    create get example

---
+++ nalu-api/src/main/kotlin/com/luizleiteoliveira/nalu/controllers/TeamController.kt
@@ -0,0 +1,17 @@
+package com.luizleiteoliveira.nalu.controllers
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping(value="/teams")
+class TeamController @Autowired constructor() {
+
+
+
+    @GetMapping
+    fun createTeam(@RequestParam (value = "name", defaultValue = "Luiz") name: String) : String {
+        return "seja bem vindo $name"
+    }
+
+}
