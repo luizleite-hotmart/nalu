ab0b2e80ce68f52b7c3bb6decfe6fb90cf20b2aa
luizleite-hotmart <luiz.leite@hotmart.com>
2018-11-05 21:54:32 -0200

    test direct on controller

--- nalu-api/src/main/kotlin/com/luizleiteoliveira/nalu/controllers/TeamController.kt
+++ nalu-api/src/main/kotlin/com/luizleiteoliveira/nalu/controllers/TeamController.kt
@@ -1,17 +1,32 @@
 package com.luizleiteoliveira.nalu.controllers
 
+import com.luizleiteoliveira.nalu.entities.Team
+import com.luizleiteoliveira.nalu.entities.Teams
+import kotlinx.nosql.mongodb.MongoDB
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping(value="/teams")
-class TeamController @Autowired constructor() {
+class TeamController @Autowired constructor(val database: MongoDB) {
 
 
 
-    @GetMapping
+    @PostMapping
     fun createTeam(@RequestParam (value = "name", defaultValue = "Luiz") name: String) : String {
-        return "seja bem vindo $name"
+        database.withSession {
+            Teams.insert(Team(name))
+        }
+        return "OK"
+    }
+
+    @PostMapping
+    fun getTeamByName(@RequestParam (value = "name", defaultValue = "Luiz") nameParam: String) : String {
+        var teams = ""
+        database.withSession({
+            val filteredTeams = Teams.find { Teams.name.matches("%$nameParam%".toPattern()) }.toList().toString()
+        })
+        return teams
     }
 
 }
