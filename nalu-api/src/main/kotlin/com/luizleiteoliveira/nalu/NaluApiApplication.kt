21387323c90d294a642816b0b11b86043ee97d99
luizleite-hotmart <luiz.leite@hotmart.com>
2018-11-05 22:11:18 -0200

    running project

--- nalu-api/src/main/kotlin/com/luizleiteoliveira/nalu/NaluApiApplication.kt
+++ nalu-api/src/main/kotlin/com/luizleiteoliveira/nalu/NaluApiApplication.kt
@@ -3,7 +3,7 @@ package com.luizleiteoliveira.nalu
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages=["com.luizleiteoliveira.nalu"])
 class NaluApiApplication
 
 fun main(args: Array<String>) {
--- nalu-api/src/main/kotlin/com/luizleiteoliveira/nalu/controllers/TeamController.kt
+++ nalu-api/src/main/kotlin/com/luizleiteoliveira/nalu/controllers/TeamController.kt
@@ -1,32 +1,18 @@
 package com.luizleiteoliveira.nalu.controllers
 
-import com.luizleiteoliveira.nalu.entities.Team
-import com.luizleiteoliveira.nalu.entities.Teams
-import kotlinx.nosql.mongodb.MongoDB
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping(value="/teams")
-class TeamController @Autowired constructor(val database: MongoDB) {
+class TeamController @Autowired constructor() {
 
 
 
-    @PostMapping
+
+    @GetMapping
     fun createTeam(@RequestParam (value = "name", defaultValue = "Luiz") name: String) : String {
-        database.withSession {
-            Teams.insert(Team(name))
-        }
-        return "OK"
-    }
-
-    @PostMapping
-    fun getTeamByName(@RequestParam (value = "name", defaultValue = "Luiz") nameParam: String) : String {
-        var teams = ""
-        database.withSession({
-            val filteredTeams = Teams.find { Teams.name.matches("%$nameParam%".toPattern()) }.toList().toString()
-        })
-        return teams
+        return "seja bem vindo $name"
     }
 
 }
