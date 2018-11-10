7d9ea57d7f55bdcadb80680ee58f92a213649466
luizleite-hotmart <luiz.leite@hotmart.com>
2018-11-10 10:16:20 -0200

    create team on controller

--- nalu-api/src/main/kotlin/com/luizleiteoliveira/nalu/controllers/TeamController.kt
+++ nalu-api/src/main/kotlin/com/luizleiteoliveira/nalu/controllers/TeamController.kt
@@ -1,19 +1,26 @@
 package com.luizleiteoliveira.nalu.controllers
 
+import com.luizleiteoliveira.nalu.entities.Team
 import com.luizleiteoliveira.nalu.repositories.TeamRepository
+
+import com.luizleiteoliveira.nalu.services.TeamService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
-
 @RestController
 @RequestMapping(value="/teams")
-class TeamController @Autowired constructor() {
+class TeamController {
 
-    val teamRepository: TeamRepository
+    private var teamService: TeamService
+
+    @Autowired
+    constructor(teamService: TeamService) {
+        this.teamService = teamService
+    }
 
     @PostMapping
-    fun createTeam(@RequestParam (value = "name", defaultValue = "Luiz") name: String) : String {
-        val newTeam = TeamRepository
-        return "seja bem vindo $name"
+    fun createTeam(@RequestParam (value = "name", defaultValue = "Luiz") name: String) : Team {
+        val newTeam = teamService.createTeam(name)
+        return newTeam
     }
 
 }
