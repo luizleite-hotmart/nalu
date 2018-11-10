fd3fb377dd1450dc9f65f3ce2438829d32e38425
luizleite-hotmart <luiz.leite@hotmart.com>
2018-11-10 10:32:21 -0200

    improve_id

--- nalu-api/src/main/kotlin/com/luizleiteoliveira/nalu/services/TeamService.kt
+++ nalu-api/src/main/kotlin/com/luizleiteoliveira/nalu/services/TeamService.kt
@@ -3,6 +3,7 @@ package com.luizleiteoliveira.nalu.services
 import com.luizleiteoliveira.nalu.entities.Team
 import com.luizleiteoliveira.nalu.repositories.TeamRepository
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class TeamService {
@@ -13,7 +14,8 @@ class TeamService {
     }
 
     fun createTeam(name: String): Team {
-        val team = Team("team-$name", name)
+        val now = Date().toInstant()
+        val team = Team("$now-$name", name)
         val returnTeam = teamRepository.insert(team)
         return returnTeam
     }
