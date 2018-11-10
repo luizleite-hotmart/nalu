efd09182071ebb6e49cfd9b5f6bafaa044b7543e
luizleite-hotmart <luiz.leite@hotmart.com>
2018-11-10 10:02:51 -0200

    improvements from our team class

--- nalu-api/src/main/kotlin/com/luizleiteoliveira/nalu/entities/Team.kt
+++ nalu-api/src/main/kotlin/com/luizleiteoliveira/nalu/entities/Team.kt
@@ -1,6 +1,11 @@
 package com.luizleiteoliveira.nalu.entities
 
+import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document(collection = "team")
-data class Team(val name: String, val id: String)
+data class Team(
+        @Id
+        val id: String,
+        val name: String
+        )
