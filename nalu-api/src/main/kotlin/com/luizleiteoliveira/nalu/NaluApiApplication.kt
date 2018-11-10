a350b18631e4da93b31277aee89fd22cc4120d8f
luizleite-hotmart <luiz.leite@hotmart.com>
2018-11-10 09:39:43 -0200

    mongo working

--- nalu-api/src/main/kotlin/com/luizleiteoliveira/nalu/NaluApiApplication.kt
+++ nalu-api/src/main/kotlin/com/luizleiteoliveira/nalu/NaluApiApplication.kt
@@ -3,7 +3,7 @@ package com.luizleiteoliveira.nalu
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(scanBasePackages=["com.luizleiteoliveira.nalu"])
+@SpringBootApplication
 class NaluApiApplication
 
 fun main(args: Array<String>) {
