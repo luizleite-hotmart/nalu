9880d2882a7d55437861be79d88f21c461b96df1
luizleite-hotmart <luiz.leite@hotmart.com>
2019-10-19 08:30:28 -0300

    add kotlin to project

---
+++ api-nalu/src/main/java/com/luizleite/entity/Operation.java
@@ -0,0 +1,4 @@
+package com.luizleite.entity;
+
+public class Operation {
+}
--- api-nalu/src/test/java/com/luizleite/service/OperationTest.java
+++ api-nalu/src/test/java/com/luizleite/service/OperationTest.java
@@ -1,9 +1,17 @@
 package com.luizleite.service;
 
+import com.luizleite.entity.Operation;
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
 
 @QuarkusTest
 public class OperationTest {
 
+    @Test
+    public void createOperationClass() {
+        Operation operation = new Operation("STOK-CODE", new Date(), 5L, 15.3, 0.5, "Broker");
+    }
 
 }
