e1d634ca0659ddafa45507036f6a3ec525c720c1
luizleite-hotmart <luiz.leite@hotmart.com>
2019-10-19 07:25:42 -0300

    create quarkus project

---
+++ api-nalu/src/test/java/com/luizleite/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package com.luizleite;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
