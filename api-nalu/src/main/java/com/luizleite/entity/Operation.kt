183e1346ad79189ef02eb067b2468ace3a66e63a
luizleite-hotmart <luiz.leite@hotmart.com>
2019-10-19 08:51:12 -0300

    create second constructor

--- api-nalu/src/main/java/com/luizleite/entity/Operation.kt
+++ api-nalu/src/main/java/com/luizleite/entity/Operation.kt
@@ -3,5 +3,8 @@ package com.luizleite.entity
 import java.util.Date
 
 class Operation(var stockName: String, var operationDate: Date,
-                var count: Long, var value: Double, var taxes: Double = 0.0,
-                var broker: String)
+                var count: Long, var value: Double, var taxes: Double,
+                var broker: String) {
+    constructor(stockName: String, operationDate: Date, count: Long, value: Double, broker: String) :
+            this(stockName, operationDate, count, value, 0.0, broker)
+}
