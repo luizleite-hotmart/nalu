652a61993e5f8976d353c7c2c7d882aff36656e4
luizleite-hotmart <luiz.leite@hotmart.com>
2019-10-19 08:47:42 -0300

    add default value for taxes

--- api-nalu/src/main/java/com/luizleite/entity/Operation.kt
+++ api-nalu/src/main/java/com/luizleite/entity/Operation.kt
@@ -3,5 +3,5 @@ package com.luizleite.entity
 import java.util.Date
 
 class Operation(var stockName: String, var operationDate: Date,
-                var count: Long, var value: Double, var taxes: Double,
+                var count: Long, var value: Double, var taxes: Double = 0.0,
                 var broker: String)
