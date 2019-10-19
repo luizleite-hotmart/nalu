5779d7c13243d5c7f62ada9200c72d213651762b
luizleite-hotmart <luiz.leite@hotmart.com>
2019-10-19 09:08:19 -0300

    rename

--- api-nalu/src/test/java/com/luizleite/entity/OperationTest.kt
+++ api-nalu/src/test/java/com/luizleite/entity/OperationTest.kt
@@ -11,8 +11,8 @@ class OperationTest {
 
     @Test
     fun `create operation class like user`() {
-        var operation = Operation("STOK-CODE", Date(), 5L, 15.3, 0.5, "Broker")
-        assertEquals(operation.stockName, "STOK-CODE")
+        var operation = Operation("STOCK-CODE", Date(), 5L, 15.3, 0.5, "Broker")
+        assertEquals(operation.stockName, "STOCK-CODE")
         assertEquals(operation.count, 5L)
         assertEquals(operation.value, 15.3)
         assertEquals(operation.taxes, 0.5)
@@ -21,8 +21,8 @@ class OperationTest {
 
     @Test
     fun `create operation class like user without tax`() {
-        var operation = Operation("STOK-CODE", Date(), 5L, 15.3, "Broker")
-        assertEquals(actual = operation.stockName, expected = "STOK-CODE")
+        var operation = Operation("STOCK-CODE", Date(), 5L, 15.3, "Broker")
+        assertEquals(actual = operation.stockName, expected = "STOCK-CODE")
         assertEquals(actual = operation.count, expected = 5L)
         assertEquals(actual = operation.value, expected = 15.3)
         assertEquals(actual = operation.taxes, expected = 0.0)
@@ -31,8 +31,19 @@ class OperationTest {
 
     @Test
     fun `create operation class total operation`() {
-        var operation = Operation("STOK-CODE", Date(), 5L, 15.3, 0.5, "Broker")
+        var operation = Operation("STOCK-CODE", Date(), 5L, 15.3, 0.5, "Broker")
         assertEquals(actual = operation.calcTotal(), expected = 77.0)
     }
 
+    @Test
+    fun `create operation class total operation without taxes`() {
+        var operation = Operation("STOCK-CODE", Date(), 5L, 15.3, "Broker")
+        assertEquals(actual = operation.calcTotal(), expected = 76.5)
+    }
+
+    @Test
+    fun `create operation class total operation add field`() {
+        var operation = Operation("STOCK-CODE", Date(), 5L, 15.3, 0.5, "Broker")
+        assertEquals(actual = operation.calcTotal(), expected = 77.0)
+    }
 }
